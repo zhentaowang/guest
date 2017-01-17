29e621cff4b835e5ff8ade7cd58ff56b9c1dee68
王振涛 <wangzhentao@zhiweicloud.com>
2017-01-17 11:21:06 +0800

    产品模块：修改项目结构，增加serv-list接口 协议模块：修改项目结构，修改协议添加与修改接口
    Differential Revision: http://121.40.150.76/D665

---
+++ protocol_manage/src/main/java/com/zhiweicloud/Application.java
@@ -0,0 +1,19 @@
+package com.zhiweicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhangpengfei on 2016/12/14.
+ */
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
