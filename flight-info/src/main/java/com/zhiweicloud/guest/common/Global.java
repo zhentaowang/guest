b68714aaed23a89b073c6837f8a90f954f3d4c0f
zhangpengfei <zhangpengfei@zhiweicloud.com>
2017-02-06 15:05:06 +0800

    航班信息

---
+++ flight-info/src/main/java/com/zhiweicloud/guest/common/Global.java
@@ -0,0 +1,16 @@
+package com.zhiweicloud.guest.common;
+
+public class Global {
+	public static String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAJMFzKLC8AB0d/k4qkl5OXRTRNHgVVGbNcWyA/Od1V1zozsVX5PET2PTn3Pi08F3RuYWiunb39t5UglOKH4yhCZfFswthHVt8K5KtujhcWn/YU10u63qZFtyO5hY8bs25PluR3mu+sN1Uf6nJuOegtTrewYTDH1qZT1fab/xc/9JAgMBAAECgYBIG60NGwz5PCZnjno+hP0/52M2iI9v8e8Cole70WqHZiLl5Iq2/65saKuo+9Qd7DFSwjWpk1SM4UjyFWpY0s/q/bNf8qwTwt4J8cSNLJ9To5kYRV0cJqt2dpF3EYwZYVMXAW5a703om26480xbpKA3bLeogs2CstX+L+DC3xZvMQJBAN/D8SHxJ7VJORxA1aUxR7Q4HmoEWfwDro+oCYI84J8th6Teyz1UlR+1+DvxMWJJxyyUuV+nEUrHdYXRhhGhYAUCQQCoM7m+4sr8aLEVVmi7XdCy5kB8/IbJIZSJoWUhBWHxxDNpzyoeNRAtrmdkrS3Da3nY3vJXQ4ONUhAWlBo9qjl1AkBEjnTfcFI5IbEErfyG+x074wG2e5TSOJKP4uze4SX0vu9THQtpw/NYXtkqDFCUC4ShbQOKgdYzcf7YvAP2fNm9AkAiasMdHgbu8vUZKp8vh5jGonqbmIhz610bdFzfDtzlvYHPqsYxfCYdUsxxO0Zb8Ef0alooJPZsGliUZiWgbI5pAkA4LcX7cyHhQSzDXe9hRUSL3vhjkG5cXrXNDup2zsfcZO0Pv4XYatK+8VAlZxhtyskqUwuHi93OxrcL1Xu+x8jX";
+
+	public static String getPrivateKey() {
+		return privateKey;
+	}
+
+	public static void setPrivateKey(String privateKey) {
+		Global.privateKey = privateKey;
+	}
+
+	
+	
+}
