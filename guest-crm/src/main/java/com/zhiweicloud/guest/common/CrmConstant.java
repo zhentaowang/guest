2926f8eff3296fcdcec6e5911a7709860f9db034
zhengyiyin <zhengyiyin@zhiweicloud.com>
2017-03-18 16:44:03 +0800

    modify  crm and orderSercive

--- guest-crm/src/main/java/com/zhiweicloud/guest/common/CrmConstant.java
+++ guest-crm/src/main/java/com/zhiweicloud/guest/common/CrmConstant.java
@@ -20,7 +20,39 @@ public class CrmConstant {
     public static final Short CARD_TYPE_SILVER  = 1;
     public static final String CARD_TYPE_SILVER_NAME = "银卡";
 
+    /**
+     * 协议类型
+     */
+    public static final String protocol_type_1 = "1";//冠名领导
+    public static final String protocol_type_2 = "2";//冠名持卡
+    public static final String protocol_type_3 = "3";//服务会员
+    public static final String protocol_type_4 = "4";//地方政要
+    public static final String protocol_type_5 = "5";//机场接待
+    public static final String protocol_type_6 = "6";//副部级政要
+    public static final String protocol_type_7 = "7";//包量客户
+    public static final String protocol_type_8 = "8";//结算会员
+    public static final String protocol_type_9 = "9";//结算金银卡
+    public static final String protocol_type_10 = "10";//结算头等舱
+    public static final String protocol_type_11 = "11";//零散散客
 
+    /**
+     * 协议类型名
+     */
+    public static final String protocol_type_name_1 = "冠名领导";
+    public static final String protocol_type_name_2 = "冠名持卡";
+    public static final String protocol_type_name_3 = "服务会员";
+    public static final String protocol_type_name_4 = "地方政要";
+    public static final String protocol_type_name_5 = "机场接待";
+    public static final String protocol_type_name_6 = "副部级政要";
+    public static final String protocol_type_name_7 = "包量客户";
+    public static final String protocol_type_name_8 = "结算会员";
+    public static final String protocol_type_name_9 = "结算金银卡";
+    public static final String protocol_type_name_10 = "结算头等舱";
+    public static final String protocol_type_name_11 = "零散散客";
+
+    /**
+     * 标签
+     */
     public static final String LABEL_NAME_1 = "头等舱";
     public static final String LABEL_NAME_2 = "金银卡";
     public static final String LABEL_NAME_3 = "地方政要";
