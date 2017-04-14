983f70b95e5754ff5c592811fc527150d36d7813
zhengyiyin <zhengyiyin@zhiweicloud.com>
2017-04-14 10:53:06 +0800

    arcnist test
    Differential Revision: http://121.40.150.76/D1088

--- guest-crm/src/main/java/com/zhiweicloud/guest/service/PassengerService.java
+++ guest-crm/src/main/java/com/zhiweicloud/guest/service/PassengerService.java
@@ -6,11 +6,9 @@ import com.zhiweicloud.guest.mapper.PassengerMapper;
 import com.zhiweicloud.guest.model.Passenger;
 import com.zhiweicloud.guest.model.PassengerQuery;
 import com.zhiweicloud.guest.model.ServiceInfo;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -24,13 +22,22 @@ public class PassengerService {
     @Autowired
     private PassengerMapper passengerMapper;
 
+    /**
+     * 分页查询
+     * @param passengerQuery
+     * @param page
+     * @param rows
+     * @author E.in
+     * @return
+     * @throws Exception
+     */
     public LZResult<PaginationResult<Passenger>> getPassengerList(PassengerQuery passengerQuery, int page, int rows) throws Exception{
         int total = passengerMapper.getListCount(passengerQuery);
         List<Passenger> passengerList = passengerMapper.queryPassengerList(passengerQuery,(page-1)*rows,rows);
         if(!CollectionUtils.isEmpty(passengerList)){
             for(Passenger p : passengerList){
-                //电话或身份证任一不为空，查询使用次数
-                if(p.getPhone() != null || !StringUtils.isEmpty(p.getIdentityCard())){
+                //电话不为空，查询使用次数（目前已电话为唯一标识）
+                if(p.getPhone() != null){
                     int count = passengerMapper.queryBuyTimes(p.getPhone(),p.getIdentityCard(),p.getAirportCode());
                     p.setBuyTimes(count);
                 }else{
@@ -46,6 +53,7 @@ public class PassengerService {
     /**
      * 根据id 获取用户信息
      * @param passengerId
+     * @author E.in
      * @return
      */
     public Passenger getPassengerById(Long passengerId,String airportCode) throws Exception{
@@ -59,8 +67,9 @@ public class PassengerService {
     }
 
     /**
-     * 获取手机或者身份证匹配的 服务信息
-     * 手机号 身份证为空的话 获取 crmPassengerId 的服务信息
+     * 获取手机号相同的 服务信息
+     * 手机号的话 获取 crmPassengerId 的服务信息
+     * @author E.in
      * @param param
      * @return
      */
