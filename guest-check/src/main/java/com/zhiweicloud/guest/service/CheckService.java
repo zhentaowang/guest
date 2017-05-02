7ea80bbb4d98d6c46e67d4ac93d272b21547d2ee
zhangpengfei <zhangpengfei@iairportcloud.com>
2017-05-02 14:49:34 +0800

    添加 汇总费用的功能

--- guest-check/src/main/java/com/zhiweicloud/guest/service/CheckService.java
+++ guest-check/src/main/java/com/zhiweicloud/guest/service/CheckService.java
@@ -126,7 +126,7 @@ public class CheckService {
 
 
         List<Map<String, Object>> checkList = checkMapper.customerChecklist(queryCondition);
-        ArrayList<String> key = new ArrayList<>(Arrays.asList("vipPersonNum","vipPrice","accompanyPersonNum","accompanyPrice","restRoomPersonNum","restRoomPrice","securityCheckPersonNum","securityCheckPrice"));
+        ArrayList<String> key = new ArrayList<>(Arrays.asList("vipPersonNum","accompanyPersonNum","restRoomPersonNum","securityCheckPersonNum","totalAmount"));
         Map<String, Object> totalRow = new HashMap<>();
 
         for(int k = 0; k < checkList.size();k++){
@@ -216,9 +216,7 @@ public class CheckService {
                 sheetName = "国际航空账单";
                 break;
         }
-        if (contentGenerator != null) {
-            ExcelUtils.exportExcel(contentGenerator, fileName, sheetName, response);
-        }
+        ExcelUtils.exportExcel(contentGenerator, fileName, sheetName,response);
     }
 
     private List<SheetContentPo> getSpecialDateList(CheckQueryParam checkQueryParam, String airportCode, Integer page, Integer rows) throws ParseException {
