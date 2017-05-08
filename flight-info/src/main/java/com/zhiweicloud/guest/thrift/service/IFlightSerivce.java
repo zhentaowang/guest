a8deccb3bcff5ba8a73ba8cbc261773aeb48f3c4
tiecheng <tczjhz@163.com>
2017-05-08 18:21:49 +0800

    提交部分改thrift代码，方便回去看

---
+++ flight-info/src/main/java/com/zhiweicloud/guest/thrift/service/IFlightSerivce.java
@@ -0,0 +1,12 @@
+package com.zhiweicloud.guest.thrift.service;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by tc on 2017/5/5.
+ */
+public interface IFlightSerivce {
+
+    String handle(JSONObject request);
+
+}
