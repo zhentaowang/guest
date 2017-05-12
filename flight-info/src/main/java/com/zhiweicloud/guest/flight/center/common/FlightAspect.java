2ca401cc9840d1ca2cb80b7a494447972be50946
tiecheng <tczjhz@163.com>
2017-05-12 16:07:47 +0800

    修改切面

--- flight-info/src/main/java/com/zhiweicloud/guest/flight/center/common/FlightAspect.java
+++ flight-info/src/main/java/com/zhiweicloud/guest/flight/center/common/FlightAspect.java
@@ -5,11 +5,10 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.zhiweicloud.guest.mapper.ExchangeDragonMapper;
 import com.zhiweicloud.guest.model.Flight;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -21,7 +20,6 @@ import java.util.List;
 @Aspect
 public class FlightAspect{
 
-//    @Pointcut("execution(* com.zhiweicloud.guest.flight.center.common.IbeUtils.queryFlightNoByDate(String,String)) and args(flightNo,flightDate)")
     @Pointcut("execution(* com.zhiweicloud.guest.thrift.util.FlightUtils.flightInfo(..))")
     public void pointcutFlightInfo(){
 
@@ -35,18 +33,42 @@ public class FlightAspect{
     /**
      * 龙腾接口
      */
+    @Autowired
     private ExchangeDragonMapper exchangeDragonMapper;
 
     @Around(value = "pointcutFlightInfo()")
-    public void around1(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object aroundFlightInfo(ProceedingJoinPoint joinPoint){
         System.out.println(joinPoint.getSignature().getName());
         JSONObject object = (JSONObject) joinPoint.getArgs()[0];
         String fnum = object.getString("fnum");
         String date = object.getString("date");
         String airportCode = object.getString("client_id");
         Long userId = object.getLong("user_id");
-        Object proceed = joinPoint.proceed();
+        Object proceed = null;
+        try {
+            proceed = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         List<Flight> flightList = JSONArray.parseArray(JSON.parseObject(proceed.toString()).getString("data"), Flight.class);
+        return proceed;
+    }
+
+    @Around(value = "pointcutCustomFlight()")
+    public Object aroundCustomFlight(ProceedingJoinPoint joinPoint){
+        System.out.println(joinPoint.getSignature().getName());
+        JSONObject object = (JSONObject) joinPoint.getArgs()[0];
+        Long flightId = object.getLong("flightId");
+        String airportCode = object.getString("client_id");
+        Long userId = object.getLong("user_id");
+        Object proceed = null;
+        try {
+            proceed = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        List<Flight> flightList = JSONArray.parseArray(JSON.parseObject(proceed.toString()).getString("data"), Flight.class);
+        return proceed;
     }
 
 }
