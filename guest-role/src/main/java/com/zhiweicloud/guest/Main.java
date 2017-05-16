56a0a85d2fae73b5b8aba05bf776d8130430469a
zhangpengfei <zhangpengfei@iairportcloud.com>
2017-05-16 09:41:18 +0800

    所有端口都改为8080

--- guest-role/src/main/java/com/zhiweicloud/guest/Main.java
+++ guest-role/src/main/java/com/zhiweicloud/guest/Main.java
@@ -1,13 +1,14 @@
 package com.zhiweicloud.guest;
 
+import com.zhiweicloud.guest.server.Server;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 /**
  * Created by luojing@wyunbank.com on 02/05/2017.
  */
 public class Main {
-    public static final int SERVER_PORT = 8101;
     public static void main(String[] args) throws InterruptedException {
+        Server.SERVER_PORT = 8080;
         GenericXmlApplicationContext context = new GenericXmlApplicationContext();
         context.getEnvironment().setActiveProfiles("production");
         context.setValidating(false);
