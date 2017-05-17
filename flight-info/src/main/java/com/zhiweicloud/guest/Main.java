5d8b981eb42b53fa85bcef8c0ae9b4a64658b90f
tiecheng <tczjhz@163.com>
2017-05-17 20:58:17 +0800

    更改航班 为最新1.9

--- flight-info/src/main/java/com/zhiweicloud/guest/Main.java
+++ flight-info/src/main/java/com/zhiweicloud/guest/Main.java
@@ -1,8 +1,12 @@
 package com.zhiweicloud.guest;
 
 import com.zhiweicloud.guest.common.Dictionary;
+
 import com.zhiweicloud.guest.server.Server;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by tc on 2017/5/5.
@@ -10,11 +14,17 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Server.SERVER_PORT = 8080;
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
         GenericXmlApplicationContext context = new GenericXmlApplicationContext();
         context.getEnvironment().setActiveProfiles("production");
         context.setValidating(false);
         context.load( "classpath:spring.xml", "classpath:mybatis.xml");
         context.refresh();
+
+        executor.execute(() -> Server.startServer());
+
         while (true) {
             System.out.println(Dictionary.START_MESSAGE);
             Thread.sleep(1000000);
