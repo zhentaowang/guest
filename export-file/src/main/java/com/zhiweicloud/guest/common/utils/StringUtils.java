1950c2edb4b7973645b85bb29707123fc42919e8
tiecheng <tczjhz@163.com>
2017-05-23 18:19:55 +0800

    导出文件 服务模块更新最新代码

---
+++ export-file/src/main/java/com/zhiweicloud/guest/common/utils/StringUtils.java
@@ -0,0 +1,20 @@
+package com.zhiweicloud.guest.common.utils;
+
+/**
+ * Created by tc on 2017/5/17.
+ */
+public class StringUtils {
+
+    public static boolean isNone(String string) {
+        if (string == null || "".equals(string) || " ".equals(string)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isNotNone(String string) {
+        return isNone(string) == true ? false : true;
+    }
+
+}
