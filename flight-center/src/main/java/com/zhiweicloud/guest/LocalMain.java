e5467cac17069f908bec4e849a367c3f76870148
tiecheng <tczjhz@163.com>
2017-06-13 16:02:42 +0800

    提交 本地main

--- flight-center/src/main/java/com/zhiweicloud/guest/LocalMain.java
+++ flight-center/src/main/java/com/zhiweicloud/guest/LocalMain.java
@@ -3,7 +3,7 @@ package com.zhiweicloud.guest;
 import com.zhiweicloud.guest.common.util.ApplicationContextUtils;
 
 /**
- * 本地测试
+ * test for localhost
  * Copyright(C) 2017 杭州风数信息技术有限公司
  *
  * 2017/6/5 20:25
@@ -13,6 +13,7 @@ public class LocalMain {
 
     public static void main(String[] args) throws InterruptedException {
         String[] strings = {"local", "thriftLocal"};
+//        ApplicationContextUtils.createContext("local");
         ApplicationContextUtils.createContext(strings);
     }
 
